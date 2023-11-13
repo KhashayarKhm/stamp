34d68c81d3259aba93ffb6ac4148e6abf7b33b9b
KhashayarKhm <khashayar.khm@protonmail.com>
2023-11-13 19:36:42 +0330

    feat(cmd/watermark): enable to watermark all images in a directory

--- cmd/watermark.go
+++ cmd/watermark.go
@@ -19,10 +19,209 @@ import (
 
 type Watermark struct{}
 
+func (wmCmd Watermark) Command(trap chan os.Signal) *cobra.Command {
+	run := func(cmd *cobra.Command, args []string) error {
+		return wmCmd.run(config.Load(true), trap, args, cmd)
+	}
+
+	cmd := &cobra.Command{
+		Use:   "watermark",
+		Short: "Watermark images",
+		Args:  cobra.ExactArgs(1),
+		RunE:  run,
+	}
+	cmd.PersistentFlags().StringP("watermark", "w", "", "specify watermark image. default: ~/.stamp/default.png")
+	cmd.PersistentFlags().StringP("output", "o", "", "output file name. default: the name of main image with \"stamped\" prefix")
+
+	return cmd
+}
+
+func (wmCmd *Watermark) run(config *config.Config, trap chan os.Signal, args []string, cmd *cobra.Command) error {
+  paths, err := extractPath(args[0])
+  if err != nil {
+    return err
+  }
+
+  skipError := len(paths) > 1
+  for _, mainPicPath := range paths {
+    if mainPicPath == "" {
+      continue
+    }
+
+    mainPicCT, err := validateImage(mainPicPath)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return err
+    }
+
+    watermarkPath, _ := cmd.Flags().GetString("watermark")
+    if watermarkPath == "" {
+      watermarkPath = config.WatermarkImg
+    }
+
+    watermarkCT, err := validateImage(watermarkPath)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return err
+    }
+
+    outputOptionValue, _ := cmd.Flags().GetString("output")
+    outputName, err := getOutputFilePath(outputOptionValue, mainPicPath)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return err
+    }
+
+    mainPicBin, err := os.Open(mainPicPath)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return errors.New("Get error on opening the main image")
+    }
+    defer mainPicBin.Close()
+
+    watermarkBin, err := os.Open(watermarkPath)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return errors.New("Get error on opening watermark image")
+    }
+    defer watermarkBin.Close()
+
+    var mainPic image.Image
+    switch mainPicCT {
+    case "image/jpeg":
+      mainPic, err = jpeg.Decode(mainPicBin)
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on decoding main image")
+      }
+    case "image/png":
+      mainPic, err = png.Decode(mainPicBin)
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on decoding main image")
+      }
+    default:
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return fmt.Errorf("The main image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", mainPicCT)
+    }
+
+    var watermarkImg image.Image
+    switch watermarkCT {
+    case "image/jpeg":
+      watermarkImg, err = jpeg.Decode(watermarkBin)
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on decoding watermark image")
+      }
+    case "image/png":
+      watermarkImg, err = png.Decode(watermarkBin)
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on decoding watermark image")
+      }
+    default:
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return fmt.Errorf("The watermark image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", watermarkCT)
+    }
+
+    offset := image.Pt(470, 50)
+    bounds := mainPic.Bounds()
+    dstImage := image.NewRGBA(bounds)
+
+    draw.Draw(dstImage, bounds, mainPic, image.ZP, draw.Src)
+    draw.Draw(dstImage, watermarkImg.Bounds().Add(offset), watermarkImg, image.ZP, draw.Over)
+
+    outputImage, err := os.Create(outputName)
+    if err != nil {
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return errors.New("Get error on creating output image")
+    }
+    defer outputImage.Close()
+
+    switch mainPicCT {
+    case "image/jpeg":
+      err = jpeg.Encode(outputImage, dstImage, &jpeg.Options{Quality: jpeg.DefaultQuality})
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on encoding output image")
+      }
+    case "image/png":
+      err = png.Encode(outputImage, dstImage)
+      if err != nil {
+        if skipError {
+          fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+          continue
+        }
+
+        return errors.New("Get error on encoding output image")
+      }
+    default:
+      if skipError {
+        fmt.Printf("Get error on %s file:\n%v\n\n", mainPicPath, err)
+        continue
+      }
+
+      return fmt.Errorf("The output image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", mainPicCT)
+    }
+  }
+
+  return nil
+}
+
 func validateImage(path string) (string, error) {
-	if file, err := os.Open(path); os.IsNotExist(err) {
-		return "", errors.New("The main picture path is not exists")
-	} else if err != nil {
+	if file, err := os.Open(path); err != nil {
 		return "", fmt.Errorf("We got an error while opening the main picture:\n%v", err)
 	} else {
 		defer file.Close()
@@ -42,124 +241,48 @@ func validateImage(path string) (string, error) {
 	}
 }
 
-func (wmCmd Watermark) Command(trap chan os.Signal) *cobra.Command {
-	run := func(cmd *cobra.Command, args []string) error {
-		return wmCmd.run(config.Load(true), trap, args, cmd)
-	}
-
-	cmd := &cobra.Command{
-		Use:   "watermark",
-		Short: "Watermark images",
-		Args:  cobra.ExactArgs(1),
-		RunE:  run,
-	}
-	cmd.PersistentFlags().StringP("watermark", "w", "", "specify watermark image. default: ~/.stamp/default.png")
-	cmd.PersistentFlags().StringP("output", "o", "", "output file name. default: the name of main image with \"stamped\" prefix")
-
-	return cmd
-}
-
-func (wmCmd *Watermark) run(config *config.Config, trap chan os.Signal, args []string, cmd *cobra.Command) error {
-	mainPicPath := args[0]
-	watermarkPath, _ := cmd.Flags().GetString("watermark")
-	if watermarkPath == "" {
-		watermarkPath = config.WatermarkImg
-	}
-
-	outputName, _ := cmd.Flags().GetString("output")
-	if outputName == "" {
-		outputName = fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath))
-  } else if exists, stat, err := internal.FileExists(outputName); err != nil {
-    return err;
+func getOutputFilePath(suggestedPath string, mainPicPath string) (string, error) {
+  if suggestedPath == "" {
+    suggestedPath = fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath))
+  } else if exists, stat, err := internal.FileExists(suggestedPath); err != nil {
+    return "", err;
   } else if exists {
     if stat.IsDir() {
-      outputName = filepath.Join(outputName, fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath)))
+      suggestedPath = filepath.Join(suggestedPath, fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath)))
     } else {
-      outputName = filepath.Join(filepath.Dir(outputName), fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath)))
+      suggestedPath = filepath.Join(filepath.Dir(suggestedPath), fmt.Sprintf("stamped_%s", filepath.Base(mainPicPath)))
     }
   }
 
-	mainPicCT, err := validateImage(mainPicPath)
-	if err != nil {
-		return err
-	}
+  return suggestedPath, nil
+}
 
-	watermarkCT, err := validateImage(watermarkPath)
-	if err != nil {
-		return err
-	}
+func extractPath(targetPath string) ([]string, error) {
+  targetPath, err := filepath.Abs(targetPath)
+  if err != nil {
+    return nil, err
+  }
 
-	mainPicBin, err := os.Open(mainPicPath)
-	if err != nil {
-    return errors.New("Get error on opening the main image")
-	}
-	defer mainPicBin.Close()
+  info, err := os.Stat(targetPath)
+  if err != nil {
+    return nil, err
+  }
 
-	watermarkBin, err := os.Open(watermarkPath)
-	if err != nil {
-		return errors.New("Get error on opening watermark image")
-	}
-	defer watermarkBin.Close()
+  if info.IsDir() {
+    entries, err := os.ReadDir(targetPath)
+    if err != nil {
+      return nil, err
+    }
 
-	var mainPic image.Image
-	switch mainPicCT {
-	case "image/jpeg":
-		mainPic, err = jpeg.Decode(mainPicBin)
-		if err != nil {
-			return errors.New("Get error on decoding main image")
-		}
-	case "image/png":
-		mainPic, err = png.Decode(mainPicBin)
-		if err != nil {
-			return errors.New("Get error on decoding main image")
-		}
-	default:
-		return fmt.Errorf("The main image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", mainPicCT)
-	}
+    subfiles := make([]string, len(entries))
+    for i, ent := range entries {
+      if !ent.IsDir() {
+        subfiles[i] = filepath.Join(targetPath, ent.Name())
+      }
+    }
 
-	var watermarkImg image.Image
-	switch watermarkCT {
-	case "image/jpeg":
-		watermarkImg, err = jpeg.Decode(mainPicBin)
-		if err != nil {
-			return errors.New("Get error on decoding watermark image")
-		}
-	case "image/png":
-		watermarkImg, err = png.Decode(watermarkBin)
-		if err != nil {
-			return errors.New("Get error on decoding watermark image")
-		}
-	default:
-		return fmt.Errorf("The watermark image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", watermarkCT)
-	}
+    return subfiles, nil
+  }
 
-	offset := image.Pt(470, 50)
-	bounds := mainPic.Bounds()
-	dstImage := image.NewRGBA(bounds)
-
-	draw.Draw(dstImage, bounds, mainPic, image.ZP, draw.Src)
-	draw.Draw(dstImage, watermarkImg.Bounds().Add(offset), watermarkImg, image.ZP, draw.Over)
-
-	outputImage, err := os.Create(outputName)
-	if err != nil {
-    return errors.New("Get error on creating output image")
-	}
-	defer outputImage.Close()
-
-	switch mainPicCT {
-	case "image/jpeg":
-		err = jpeg.Encode(outputImage, dstImage, &jpeg.Options{Quality: jpeg.DefaultQuality})
-		if err != nil {
-			return errors.New("Get error on encoding output image")
-		}
-	case "image/png":
-		err = png.Encode(outputImage, dstImage)
-		if err != nil {
-			return errors.New("Get error on encoding output image")
-		}
-	default:
-		return fmt.Errorf("The output image format unsupported. (Supported formats: jpg,jpeg,png - Entered file format: %s)", mainPicCT)
-	}
-
-	return nil
+  return []string{targetPath}, nil
 }
